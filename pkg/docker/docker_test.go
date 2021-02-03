3e9fa752f1e60ade3dd29e761278b9148c4452e1
stuart.warren <stuart.warren@ocado.com>
2021-02-03 08:52:12 +0000

    test: correct test for env flags

--- pkg/docker/docker_test.go
+++ pkg/docker/docker_test.go
@@ -10,7 +10,7 @@ func TestEnvToArgs(t *testing.T) {
 		"KEY1": "val1",
 		"KEY2": "val2",
 	}
-	expected := []string{"--env", "KEY1=\"val1\"", "--env", "KEY2=\"val2\""}
+	expected := []string{"--env", "KEY1=val1", "--env", "KEY2=val2"}
 	args := envToDockerArgs(env)
 	if !reflect.DeepEqual(expected, args) {
 		t.Errorf("got: %v, expected: %v", args, expected)
